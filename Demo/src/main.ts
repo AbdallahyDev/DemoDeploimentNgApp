0658cb7f8c2d1f78f2bd88fd19f0d6694b7319f2
AbdallahyDev <abdallahy.bilal@gmail.com>
2017-11-14 18:17:58 +0100

    fristgn

--- Demo/src/main.ts
+++ Demo/src/main.ts
@@ -3,3 +3,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/modules/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+// pour test
